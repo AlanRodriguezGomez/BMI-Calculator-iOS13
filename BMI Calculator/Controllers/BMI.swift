509f020c6ca599766e11a693dc0691f9824d1bcd
AlanRodriguezGomez <47120912+AlanRodriguezGomez@users.noreply.github.com>
2024-01-10 13:17:39 -0600

    Cambios
    * Se sube proyecto completo.

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Alan Josue Rodriguez Gomez on 13/12/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
